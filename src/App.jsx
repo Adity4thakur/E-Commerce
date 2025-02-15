1a678a45fca1be22dff50f9318900f72362a2630
aditya <athakur241998gmail.com>
2025-02-16 01:52:59 +0530

    create a product page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Home from './pages/Home'
+import Product from './pages/Product'
+// import { Category } from './pages/Category'
 
 const App = () => {
   return (
     <>
  <Header/>
- <Home/>
+ {/* <Home/> */}
+{/* <Category/> */}
+<Product/>
 <Footer/>
     </>
   )
