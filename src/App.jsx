98cdf9d8ddf699223dc2809f37ccf5c78bc105d1
aditya <athakur241998gmail.com>
2025-02-15 11:04:48 +0530

    creating home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <>
  <Header/>
- <div>
-  <h2>THis is aditya </h2>
- </div>
+ <Home/>
 <Footer/>
     </>
   )
